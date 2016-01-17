91d341428aa96ff859707d6bcb50b004c4c45dfb
Rajiv Précieux Mounguengue <rajiv.mounguengue@hotmail.fr>
2016-01-17 20:53:48 +0100

    Added entities project.

---
+++ src/back-end/core/CqrsWorkShop.Server.Core.Entities/Entities.fs
@@ -0,0 +1,10 @@
+﻿namespace Hse.CqrsWorkShop.Domain.Entities
+
+open System
+
+
+type IEntity = 
+    abstract member Id: int
+
+type OrderLine = 
+    {ProductId: Guid; ProductName: string; OriginalPrice: int; DiscountedPrice: int; Quantity: int}
