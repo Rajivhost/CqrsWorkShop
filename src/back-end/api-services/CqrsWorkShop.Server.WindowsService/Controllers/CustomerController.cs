7a1217f9fa175228fd7a63e8251a2b77489c5e2d
Rajiv Précieux Mounguengue <rajiv.mounguengue@hotmail.fr>
2016-01-18 01:52:41 +0100

    Added CustomerController.

---
+++ src/back-end/api-services/CqrsWorkShop.Server.WindowsService/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Hse.CqrsWorkShop.Domain;
+using Hse.CqrsWorkShop.Domain.Commands;
+
+namespace Hse.CqrsWorkShop.Api.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IGuidIdProvider _guidIdProvider;
+
+        public CustomerController(ICommandDispatcher commandDispatcher, IGuidIdProvider guidIdProvider)
+        {
+            _commandDispatcher = commandDispatcher;
+            _guidIdProvider = guidIdProvider;
+        }
+
+        [HttpPost, Route("createcustomer")]
+        public IHttpActionResult Createcustomer(string name)
+        {
+            var id = _guidIdProvider.GenerateId();
+            var createCustomer = new CreateCustomer(id, name);
+
+            _commandDispatcher.ExecuteCommand(createCustomer);
+
+            return Created("createcustomer", createCustomer);
+        }
+    }
+}
